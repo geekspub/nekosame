0ff99f496c06bf5ff250d9937b852e3995872272
geekspub <124560978+geekspub@users.noreply.github.com>
2023-02-18 00:49:49 +0530

    commit1

---
+++ script.js
@@ -0,0 +1,29 @@
+let nameInput = document.getElementById('name');
+let btn = document.querySelector('.name-btn');
+let text = document.querySelector('.prompt-text');
+let prompt = document.querySelector(".prompt");
+
+btn.addEventListener('click', () => {
+    let name = nameInput.value;
+    console.log(name, "name")
+    nameInput.value = "";
+    
+    if (name == "shivani" || name == "shivani muthoju")
+    {
+        text.innerText = "Emi ra Balraju Emi ra Deshaniki ne valla upayogam";
+        prompt.style.display = "grid";
+        closePrompt();
+    }
+    else {
+        text.innerText = "Yedhava te te lu adaku per kottu";
+        prompt.style.display = "grid";
+        closePrompt();
+    }
+    
+})
+
+function closePrompt() {
+    setTimeout(() => {
+        prompt.style.display = "none";
+    },5000)
+}
